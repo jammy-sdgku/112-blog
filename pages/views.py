f0c2863e0319bc0b8bdf1f164026e7764731163d
jammy-sdgku <james.jones@sdgku.edu>
2025-09-11 16:23:33 +0100

    112 Assignment 1

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+#home page
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+    
+#about page
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
