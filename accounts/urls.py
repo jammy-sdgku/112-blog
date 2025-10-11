dfdd8d1e983aea7621ef119775f9f0054305293a
jammy-sdgku <james.jones@sdgku.edu>
2025-10-11 16:50:03 +0100

    register  user accounts and authentication system

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import AccountCreateView, AccountDetailView
 
 urlpatterns = [
-   
+   path('new/', AccountCreateView.as_view(), name='account_new'), 
+   path('detail/', AccountDetailView.as_view(), name='account_detail'),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,24 @@
+from django.contrib.auth.models import User  # Use Django's built-in User model
 from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class AccountCreateView(CreateView):
+    model = User
+    template_name = 'accounts/new.html'
+    fields = ['username', 'last_name', 'email', 'password']
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])  # Hash the password
+        user.save()
+        return super().form_valid(form)
+
+class AccountDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'accounts/detail.html'
+    context_object_name = 'account'
+
+    def get_object(self, queryset=None):
+        return self.request.user
